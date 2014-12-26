6bde6644b7cf0ec2ec29dcb78c33bc60368e9f26
taukir4u <taukir4u@gmail.com>
2014-12-26 19:39:41 +0600

    C#
    Signed-off-by: taukir4u <taukir4u@gmail.com>

---
+++ ConvertCharecterToValue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertCharecterToValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char charecter;
+            int value;
+            charecter = Convert.ToChar(Console.ReadLine());
+            value = Convert.ToInt32(charecter);
+            Console.WriteLine(value);
+            Console.ReadKey();
+
+        }
+    }
+}
